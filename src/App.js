064e4edac108351d5ccc68f7aa0f4c932ad9a177
doikazuma <doi.kazuma@tis.co.jp>
2023-02-28 16:41:07 +0900

    webview検証用のコード

--- src/App.js
+++ src/App.js
@@ -1,11 +1,37 @@
-import logo from './logo.svg';
 import './App.css';
+import logo from './logo.svg';
+import { useCallback, useState } from 'react';
 
 function App() {
+  const [text, setText] = useState('Hello');
+  const onChange = useCallback((event) => setText(event.target.value),[]);
+  const onCopy = useCallback(() => {
+    navigator.clipboard.writeText(text);
+  }, [text]);
+  const onPaste = useCallback(async() => {
+    const readText = await navigator.clipboard.readText();
+    setText(readText);
+  },[]);
+  const onNativeCopy = useCallback(() => {
+    console.log('onNativeCopy')
+    window.ReactNativeWebView.postMessage(text);
+  },[text]);
+  const onNativePaste = useCallback(async() => {
+    const readText = await window.ReactNativeWebView.target.value;
+    console.log('readText: ' + readText);
+    setText(readText);
+  },[]);
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <input type="text" value={text} onChange={onChange}></input>
+        {/* <button onClick={onCopy}>copy</button> */}
+        <button onClick={onNativeCopy}>native copy</button>
+        <button onClick={onNativePaste}>native paste</button>
+        {/* <button onClick={onPaste}>paste</button> */}
+
+        {/* <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
@@ -16,7 +42,7 @@ function App() {
           rel="noopener noreferrer"
         >
           Learn React
-        </a>
+        </a> */}
       </header>
     </div>
   );
